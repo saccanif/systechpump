2576d3820a93255c4efaf851bb2c6292dec2ecd9
Bertte-Buged <enzobschulz@gmail.com>
2025-10-01 18:59:29 -0300

    Create alterarLojaRepre.php

---
+++ public/alterarLojaRepre.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <title>Alterar Loja - Systech Pump</title>
+  <link rel="stylesheet" href="./css/representanteHome.css" />
+  <link rel="stylesheet" href="./css/cadastroStores.css" />
+    <link rel="stylesheet" href="./css/cadastroCidades.css" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+  <link rel="shortcut icon" href="./imgs/others/logo.png" type="image/png" />
+</head>
+<body>
+  <div class="sidebar">
+    <div class="user">
+      <img src="./imgs/others/representante.jpg" alt="Usuário" />
+      <span>representante</span>
+    </div>
+    <ul>
+      <li><a href="./representanteHome.php"><i class="fa fa-home"></i> Home</a></li>
+      <li><a href="#"><i class="fa fa-store"></i> Pedidos</a></li>
+      <li><a href="./cadastroStoresRepre.php"><i class="fa fa-cart-plus"></i> Cadastrar lojas</a></li>
+      <li><a href="./cadastroCidadesRepre.php"><i class="fa fa-city"></i> Cadastrar Cidades</a></li>
+      <li><a href="./deletarLojaRepre.php"><i class="fa fa-delete"></i> Deletar Lojas</a></li>
+      <li><a href="./alterarLojaRepre.php"><i class="fa fa-alter"></i> Alterar Lojas</a></li>
+    </ul>
+    <div class="bottom-menu">
+      <ul>
+        <li><a href="./config.php"><i class="fa fa-home"></i> Configurações</a></li>
+        <li><a href="./index.php"><i class="fa fa-store"></i> Sair</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="topbar">
+      <h2>Alterar Loja</h2>
+      <div class="logo">
+        <img src="./imgs/others/logo-branco.png" alt="logo loja" />
+      </div>
+    </div>
+
+        <?php
+          if (isset($_GET["msg"])) {
+            $mensagem = $_GET["msg"];
+            echo "<font color=red>$mensagem</font>";
+          }
+        ?>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+
