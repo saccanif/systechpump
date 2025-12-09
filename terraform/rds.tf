312d73385043abd60c021e2d897adae3f59bfab6
saccanif <165558713+saccanif@users.noreply.github.com>
2025-12-09 08:06:20 -0300

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,11 +1,18 @@
 resource "aws_db_instance" "rds" {
-  identifier           = "systechpump"
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = var.db_username
-  password             = var.db_password
-  skip_final_snapshot  = true
-  publicly_accessible  = false
+  identifier            = "systechpump"
+  allocated_storage     = 20
+  engine                = "mysql"
+  engine_version        = "8.0"
+  instance_class        = "db.t3.micro"
+  username              = var.db_username
+  password              = var.db_password
+  skip_final_snapshot   = true
+  publicly_accessible   = true
+
+  # db_subnet_group_name  = aws_db_subnet_group.rds_subnet_group.name
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+
+  vpc_security_group_ids = [
+    aws_security_group.rds_sg.id
+  ]
 }
