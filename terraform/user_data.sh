4767af3964ea9ea10527c7e3b0381159d436d11e
saccanif <165558713+saccanif@users.noreply.github.com>
2025-12-09 08:08:15 -0300

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 yum update -y
-yum install -y httpd php git mariadb -y
+yum install -y httpd php php-mysqli git
+yum install -y mariadb105
 
+sudo systemctl enable httpd
+sudo systemctl start httpd
 
-# echo "DB_HOST='${db_endpoint}'" > /var/www/html/config.php
+DB_ENDPOINT="${db_endpoint}"
+DB_USERNAME="${db_username}"
+DB_PASSWORD="${db_password}"
 
-rm -rf /var/www/html/
-
+rm -rf /var/www/html
 git clone https://github.com/saccanif/systechpump.git /var/www/html
 
-cd /var/www/html
-
-cat > index.php << EOF
-<?php
-header("Location: ./public/index.php");
-?>
+cat > /var/www/html/index.php << EOF
+<?php header("Location: ./public/index.php"); ?>
 EOF
 
-mysql -u ${db_username} -p${db_password} -h ${db_endpoint} systechpump < /var/www/html/database/systechpump.sql
+echo "Esperando o RDS ficar pronto..." > /root/rds.log
 
-rm ./config/connection.php
+# # Espera até conseguir conectar
+# until mysql -h "$db_endpoint" -P 3306 -u "$db_username" -p"$db_password" -e "SELECT 1" &> /dev/null
+# do
+#   echo "$(date) - RDS ainda não está disponível" >> /root/rds.log
+#   sleep 10
+# done
 
-cat > ./config/connection.php << EOF
+echo "RDS pronto" >> /root/rds.log
+
+# mysql -h "$DB_ENDPOINT" -P 3306 -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS systechpump;" >> /root/rds.log
+mysql -h "${db_endpoint}" -P 3306 -u "${db_username}" -p"${db_password}"  < /var/www/html/database/systechpump.sql 
+
+
+rm -f /var/www/html/config/connection.php
+
+cat > /var/www/html/config/connection.php << EOF
 <?php
-   function conectarBD(){
-        $conexao = mysqli_connect("${db_endpoint}","${db_username}","${db_password}","systechpump") or die ("Erro ao conectar com o Banco!");
-        
-        mysqli_query($conexao, "SET NAMES 'utf8'");
-        mysqli_query($conexao, "SET character_set_connection=utf8");
-        mysqli_query($conexao, "SET character_set_client=utf8");
-        mysqli_query($conexao, "SET character_set_results=utf8");
+function conectarBD(){
+    \$conexao = mysqli_connect("${db_endpoint}", "${db_username}", "${db_password}", "systechpump");
 
-        return $conexao;
-    }
+    mysqli_query(\$conexao, "SET NAMES 'utf8'");
+    mysqli_query(\$conexao, "SET character_set_connection=utf8");
+    mysqli_query(\$conexao, "SET character_set_client=utf8");
+    mysqli_query(\$conexao, "SET character_set_results=utf8");
 
-    $conexao = conectarBD();
+    return \$conexao;
+}
+
+    \$conexao = conectarBD();
 ?>
 EOF
-
-systemctl enable httpd
-systemctl start httpd
