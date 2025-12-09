b9e84333c82112532cce581d5801cd5874974b79
saccanif <165558713+saccanif@users.noreply.github.com>
2025-12-09 08:06:41 -0300

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,8 +1,9 @@
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
-  description = "Allow SSH and HTTP"
+  description = "Allow HTTP and SSH from anywhere"
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -10,9 +11,18 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description     = "MySQL"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -21,5 +31,9 @@ resource "aws_security_group" "web_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
   }
 }
