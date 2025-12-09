bed2551072963816436810af1e85a10db91cd34f
saccanif <165558713+saccanif@users.noreply.github.com>
2025-12-09 08:05:28 -0300

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,12 +4,17 @@ resource "aws_instance" "web" {
     aws_db_instance.rds
   ]
 
-  ami                    = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2
+subnet_id = data.aws_subnet.subnet_1.id
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  ami                    = "ami-0fa3fe0fa7920f68e"
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = templatefile("user_data.sh" , {
-    db_endpoint = aws_db_instance.rds.endpoint
+    db_endpoint = aws_db_instance.rds.address
     db_username = var.db_username
     db_password = var.db_password
   })
@@ -17,4 +22,5 @@ resource "aws_instance" "web" {
   tags = {
     Name = "systechpump"
   }
+
 }
